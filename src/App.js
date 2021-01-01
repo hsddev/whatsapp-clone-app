7e0ba9b5cc92c2ab446e3a6bd50931bdcd8ae6f9
hsddev <hsd.salaheddine@gmail.com>
2021-01-01 18:14:59 +0000

    add React router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-        {/* Sidebar */}
-        <Sidebar />
-        {/* Chat */}
-        <Chat />
-        {/* */}
+        <Router>
+          <Switch>
+            <Route path="/">
+              <h1>Home Screen</h1>
+            </Route>
+
+            <Route path="/app">
+              {/* Sidebar */}
+              <Sidebar />
+              {/* Chat */}
+              <Chat />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
