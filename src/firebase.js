9cf281c9a8ce1fa4f01e91b797ce2d2dd1eae1df
hsddev <hsd.salaheddine@gmail.com>
2021-01-01 14:05:22 +0000

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCK9LVNIKndd8-mgNaHEAv-XjeSY31TId4",
+  authDomain: "whatsapp-clone-7b918.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-7b918.firebaseio.com",
+  projectId: "whatsapp-clone-7b918",
+  storageBucket: "whatsapp-clone-7b918.appspot.com",
+  messagingSenderId: "77068089754",
+  appId: "1:77068089754:web:9c5e3b7de9073662316223",
+  measurementId: "G-9YESLP0KHS",
+};
